5e8e579c5067c8eb3a4a0086d619850d67af29fb
Adam Talbot <adam.talbot@skybettingandgaming.com>
2016-09-16 17:12:57 +0100

    Bundled clean into stop

--- swarm-start.sh
+++ swarm-start.sh
@@ -7,12 +7,14 @@ docker run -h swarm-slave3 --privileged --name=swarm-slave3 --rm docker:1.12-din
 
 sleep 1
 
-docker exec swarm-master docker swarm init
-docker exec swarm-master docker run -d -p 3000:3000 -e HOST=localhost -e PORT=3000 -v /var/run/docker.sock:/var/run/docker.sock manomarks/visualizer
+docker exec swarm-master docker swarm init > /dev/null 2> /dev/null && echo "swarm-master initialized"
+
+echo "pulling visualizer image"
+docker exec swarm-master docker run -d -p 3000:3000 -e HOST=localhost -e PORT=3000 -v /var/run/docker.sock:/var/run/docker.sock manomarks/visualizer > /dev/null 2> /dev/null && echo "visualizer started"
 
 TOKEN=`docker exec swarm-master docker swarm join-token worker -q`
 IP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' swarm-master`
 
-docker exec swarm-slave1 docker swarm join --token $TOKEN $IP:2377
-docker exec swarm-slave2 docker swarm join --token $TOKEN $IP:2377
-docker exec swarm-slave3 docker swarm join --token $TOKEN $IP:2377
+docker exec swarm-slave1 docker swarm join --token $TOKEN $IP:2377 > /dev/null 2> /dev/null && echo "swarm-slave1 joined"
+docker exec swarm-slave2 docker swarm join --token $TOKEN $IP:2377 > /dev/null 2> /dev/null && echo "swarm-slave2 joined"
+docker exec swarm-slave3 docker swarm join --token $TOKEN $IP:2377 > /dev/null 2> /dev/null && echo "swarm-slave2 joined"
