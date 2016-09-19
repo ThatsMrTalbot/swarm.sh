8414a742412e1a3e4b7f79e41ecc9f71a0c5cd99
Adam Talbot <adam.talbot@skybettingandgaming.com>
2016-09-19 10:50:53 +0100

    Moved visualizer to seperate script

--- swarm-start.sh
+++ swarm-start.sh
@@ -31,6 +31,3 @@ IP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' swarm-master`
 docker exec swarm-slave1 docker swarm join --token $TOKEN $IP:2377 > /dev/null 2> /dev/null && echo "swarm-slave1 joined"
 docker exec swarm-slave2 docker swarm join --token $TOKEN $IP:2377 > /dev/null 2> /dev/null && echo "swarm-slave2 joined"
 docker exec swarm-slave3 docker swarm join --token $TOKEN $IP:2377 > /dev/null 2> /dev/null && echo "swarm-slave2 joined"
-
-echo "pulling visualizer image"
-docker exec swarm-master docker run -d -p 3000:3000 -e HOST=localhost -e PORT=3000 -v //var/run/docker.sock://var/run/docker.sock manomarks/visualizer > /dev/null 2> /dev/null && echo "visualizer started"
